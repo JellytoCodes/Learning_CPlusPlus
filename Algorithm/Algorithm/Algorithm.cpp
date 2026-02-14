a3e26df739d70a721caf5e315411f767283d529b
JellytoCodes <taegu234@naver.com>
2026-02-14 16:25:40 +0900

    LIS (Longest Increasing Sequence) 예제 문제

--- Algorithm/Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm/Algorithm.cpp
@@ -1,34 +1,44 @@
 ﻿#include <iostream>
 #include <thread>
 #include <Windows.h>
+#include <vector>
+using namespace std;
 
-// 메모이제이션 (memoization)
-int cache[50][50];
+// LIS (Longest Increasing Sequence)
+int cache[100];
+vector<int> seq;
 
-
-int combination(int n, int r)
+int LIS(int pos)
 {
-	// 기저 사례
-	if (r == 0 || n == r)
-		return 1;
+	// 기저 사항
+	//if (pos == seq.size() - 1)
+	//	return 1;
 
-	// 이미 답을 구한 적 있으면 바로 반환
-	int& ret = cache[n][r];
+	// 캐시 확인
+	int& ret = cache[pos];
 	if (ret != -1)
 		return ret;
 
-	// 새로 답을 구해서 캐시에 저장
-	return ret = combination(n -1, r - 1) + combination(n - 1, r);
+	// 구하기
+
+	// 최소 seq[pos]은 있으니 1부터 시작
+	ret = 1;
+
+	for (int next = pos + 1 ; next < seq.size() ; next++)
+	{
+		if (seq[pos] < seq[next])
+			ret = max(ret, 1 + LIS(next));
+	}
+
+	return ret;
 }
 
 int main()
 {
 	::memset(cache, -1, sizeof(cache));
-	__int64 start = GetTickCount64();
+	seq = vector<int>{10, 1, 9, 2, 5, 7};
 
-	int lotto = combination(45, 6);
-
-	__int64 end = GetTickCount64();
-
-	std::cout << end - start << "\n";
+	int ret = 0;
+	for (int pos = 0 ; pos < seq.size() ; pos++)
+		ret = max(ret, LIS(0));
 }
